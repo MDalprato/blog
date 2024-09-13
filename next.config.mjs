c7f557b1127c11cb38052a44ddb3cf6a5bd45c44
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-09-13 17:46:37 +0200

    bhoooo

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,9 @@ const nextConfig = {
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
     basePath: "/blog",
-  
+    assetPrefix:"/blog",
+
+
     /**
      * Disable server-based image optimization. Next.js does not support
      * dynamic features with static exports.
