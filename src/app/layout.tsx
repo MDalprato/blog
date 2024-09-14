fe7995db71effc16004288016412c63e3d83fdcc
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-09-14 23:42:57 +0200

    ooook maybe to late

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
+import { BLOG_AUTHOR, BLOG_NAME } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Next.js Blog Example with ${BLOG_NAME}`,
+  description: `A statically generated blog example using Next.js and ${BLOG_AUTHOR}.`,
   openGraph: {
-    images: [HOME_OG_IMAGE_URL],
+    images: [],
   },
 };
 
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,13 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
 import Alert from "@/app/_components/alert";
 import Container from "@/app/_components/container";
 import Header from "@/app/_components/header";
 import { PostBody } from "@/app/_components/post-body";
 import { PostHeader } from "@/app/_components/post-header";
+import { BLOG_AUTHOR } from "@/lib/constants";
 
 export default async function Post({ params }: Params) {
   const post = getPostBySlug(params.slug);
@@ -50,7 +50,7 @@ export function generateMetadata({ params }: Params): Metadata {
     return notFound();
   }
 
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | Next.js Blog Example with ${BLOG_AUTHOR}`;
 
   return {
     title,
