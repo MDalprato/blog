0212968bd027f409c9c7765877ebdea38339756d
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-09-15 11:28:43 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = process.env.LOCAL === 'true' ? '' : '/blog';
 
 const nextConfig = {
   output: "export",
-  basePath: basePath, // Il nome del tuo repository GitHub
+  basePath: "/blog", // Il nome del tuo repository GitHub
   assetPrefix: "", // Imposta questo prefisso per caricare correttamente gli asset
   images: {
     path: "", // Percorso corretto per le immagini in base al repository
